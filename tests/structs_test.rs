fc047a6119228fa18e68c0955b5918f31dac9d65
Tosiek <antonizieciak@gmail.com>
2023-05-08 17:07:36 +0200

    Added tests for interfaces.

--- tests/structs_test.rs
+++ tests/structs_test.rs
@@ -104,4 +104,40 @@ mod structs_test {
         assert_eq!(new_class.get_attributes().len(), 3);
         assert_eq!(new_class.get_methods().len(), 3);
     }
+
+    #[test]
+    fn parse_interface_with_only_components() {
+        let input: &str = "interface klasa1 {\n\tattributes {\n\t\t- atrybut_prywatny : typ\n\t\t+ atrybut_publiczny : typ\n\t\t# atrybut_chroniony : typ\n\t}\n}\n";
+        let class = GrammarParser::parse(Rule::INTERFACE, input)
+            .unwrap().next().unwrap();
+        let new_class = Class::new(class.clone(), true);
+        assert_eq!(new_class.get_keyword().to_owned(), String::from("interface"));
+        assert_eq!(new_class.get_name().to_owned(), String::from("klasa1"));
+        assert_eq!(new_class.get_attributes().len(), 3);
+        assert_eq!(new_class.get_methods().len(), 0)
+    }
+
+    #[test]
+    fn parse_interface_with_only_methods() {
+        let input: &str = "interface klasa1 {\n\tmethods {\n\t\t- metoda_prywatna : typ\n\t\t+ metoda_publiczna : typ\n\t\t# metoda_chroniona\n\t}\n}\n";
+        let class = GrammarParser::parse(Rule::INTERFACE, input)
+            .unwrap().next().unwrap();
+        let new_class = Class::new(class.clone(), true);
+        assert_eq!(new_class.get_keyword().to_owned(), String::from("interface"));
+        assert_eq!(new_class.get_name().to_owned(), String::from("klasa1"));
+        assert_eq!(new_class.get_attributes().len(), 0);
+        assert_eq!(new_class.get_methods().len(), 3);
+    }
+
+    #[test]
+    fn parse_interface_with_methods_and_attributes() {
+        let input: &str = "interface klasa1 {\n\tattributes {\n\t\t- atrybut_prywatny : typ\n\t\t+ atrybut_publiczny : typ\n\t\t# atrybut_chroniony : typ\n\t}\n\tmethods {\n\t\t- metoda_prywatna : typ\n\t\t+ metoda_publiczna : typ\n\t\t# metoda_chroniona\n\t}\n}\n";
+        let class = GrammarParser::parse(Rule::INTERFACE, input)
+            .unwrap().next().unwrap();
+        let new_class = Class::new(class.clone(), true);
+        assert_eq!(new_class.get_keyword().to_owned(), String::from("interface"));
+        assert_eq!(new_class.get_name().to_owned(), String::from("klasa1"));
+        assert_eq!(new_class.get_attributes().len(), 3);
+        assert_eq!(new_class.get_methods().len(), 3);
+    }
 }
