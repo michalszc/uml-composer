43a0b5e9fa33a77ffd5f04d0818444be9b4f9111
Neuman, Wojciech <wojciech.neuman@gmail.com>
2023-05-14 19:49:56 +0200

    added tests

--- tests/context_test.rs
+++ tests/context_test.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod context_test {
     use pest::Parser;
+    use svg::node::element::SVG;
     use uml_composer::{rules::context::Context, grammar_parser::{GrammarParser, Rule}};
 
     #[test]
@@ -15,6 +16,34 @@ mod context_test {
         assert_eq!(context.get_use_cases()[0].get_use_case_label().to_owned(), String::from("Eat food"));
         assert_eq!(context.get_use_cases()[1].get_use_case_alias().to_owned(), String::from("uc2"));
         assert_eq!(context.get_use_cases()[1].get_use_case_label().to_owned(), String::from("Pay for food"));
+
+        const START_SVG: &str = "<svg xmlns=\"http://www.w3.org/2000/svg\">";
+        const END_SVG: &str = "</svg>";
+        let mut svg = SVG::new();
+        let x = 50;
+        let y = 100;
+        let width = 200;
+        let height = 200;
+        context.draw(&mut svg, x, y, width, height);
+        let expected_svg = format!(
+            "{}\n\
+        <text dominant-baseline=\"central\" fill=\"black\" font-size=\"28\" text-anchor=\"middle\" x=\"150\" y=\"80\">\n\
+        {}\n\
+        </text>\n\
+        <rect fill=\"transparent\" fill-opacity=\"0.3\" height=\"{}\" rx=\"10\" ry=\"10\" stroke=\"gray\" stroke-width=\"2\" width=\"{}\" x=\"{}\" y=\"{}\"/>\n\
+        <ellipse cx=\"149\" cy=\"137\" fill=\"blue\" fill-opacity=\"0.2\" rx=\"58\" ry=\"30\" stroke=\"black\" stroke-width=\"2\"/>\n\
+        <text dominant-baseline=\"central\" fill=\"black\" font-size=\"20\" text-anchor=\"middle\" x=\"149\" y=\"143\">\n\
+        Eat food\n\
+        </text>\n\
+        <ellipse cx=\"149\" cy=\"237\" fill=\"blue\" fill-opacity=\"0.2\" rx=\"82\" ry=\"30\" stroke=\"black\" stroke-width=\"2\"/>\n\
+        <text dominant-baseline=\"central\" fill=\"black\" font-size=\"20\" text-anchor=\"middle\" x=\"149\" y=\"243\">\n\
+        Pay for food\n\
+        </text>\n\
+    {}",
+            START_SVG, context.get_context_label(), height, width, x, y, END_SVG
+        );
+
+        assert_eq!(svg.to_string(), expected_svg);
     }
 }
 
